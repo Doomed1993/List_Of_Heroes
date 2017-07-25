9fbf183de2e57dcdaed040597dfdbdda56ee2a3c
Doomed93 <priyadsg@gmail.com>
2017-07-25 17:07:42 +0530

    MasterSlave Pattern Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponentComponent } from './HeroDetail/hero-detail-component/hero-detail-component.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero_detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponentComponent,
     HeroDetailComponent
   ],
   imports: [
